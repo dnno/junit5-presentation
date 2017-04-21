172e38011027d9b493829de60604d13d36bc2e22
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2017-04-21 13:24:09 +0200

    Assertions demo tests

--- src/test/java/JupiterTests.java
+++ src/test/java/JupiterTests.java
@@ -1,8 +1,40 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class JupiterTests {
 
   @Test
   void helloJupiter() {
+    System.out.println("Hello Jupiter");
+  }
+
+  @Test
+  void multipleAssertions() {
+    Person person = new Person();
+    assertAll(() -> assertNotNull(person),
+        () -> assertEquals("Jane", person.firstname),
+        () -> assertEquals("Doe", person.lastname));
+  }
+
+  @Test
+  void timeouts() {
+    assertTimeout(Duration.ofMillis(10), () -> {
+      // Thread.sleep(2000);
+    });
+  }
+
+  @Test
+  void preemptiveTimeout() {
+    assertTimeoutPreemptively(Duration.ofMillis(10), () -> {
+      // Thread.sleep(2000);
+    });
+  }
+
+  static class Person {
+    String firstname = "Jane";
+    String lastname = "Doe";
   }
 }
